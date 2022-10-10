2360bb8b8f572ab6fe6f9d75d691c11bb22d3586
Luca Ghislotti <ghislottiluca@gmail.com>
2022-10-10 18:06:50 +0200

    Updated Matlab and Python

--- acquire_data.py
+++ acquire_data.py
@@ -1,10 +1,16 @@
 import numpy as np
 
-event_data = np.load("GFP_Data/numpy_data.npz")
+# TRANSFER FUNCTION (old)
 
+
+# EVENT DATA ACQUISITION
+event_data = np.load("GFP_Data/numpy_data.npz")
 adc_values = event_data["adc_values"]
 edep_values = event_data["edep_values"]
 
-print(event_data["adc_values", "edep_values"])
+print(edep_values)
+# print(event_data["adc_values", "edep_values"])
 
-# TRANSFER FUNCTION (old)
+data = np.load("GFP_Data/numpy_data.npz")
+for key, value in data.items():
+    np.savetxt("output/" + key + ".csv", value)
