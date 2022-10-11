ad3e514229289c361ae22a8f5802207fbc8abdc3
Luca Ghislotti <ghislottiluca@gmail.com>
2022-10-11 17:31:42 +0200

    Added matlab plot and python script

--- acquire_data.py
+++ acquire_data.py
@@ -1,16 +1,25 @@
 import numpy as np
 
-# TRANSFER FUNCTION (old)
-
-
 # EVENT DATA ACQUISITION
 event_data = np.load("GFP_Data/numpy_data.npz")
 adc_values = event_data["adc_values"]
 edep_values = event_data["edep_values"]
 
-print(edep_values)
-# print(event_data["adc_values", "edep_values"])
+# print(adc_values[:, 0, 0, :])
 
-data = np.load("GFP_Data/numpy_data.npz")
-for key, value in data.items():
-    np.savetxt("output/" + key + ".csv", value)
+file = open("GFP_Data/events/row0_mod0_allch.dat", "w+")
+
+for ch in range(0, 32):
+    array = np.array(adc_values[:, 0, 0, ch])
+
+    count = 0
+    list = []
+    for e in array:
+        if e != 0:
+            count = count + 1
+            list.append(e)
+
+    for item in list:
+        file.write(str(item) + ", ")
+
+    file.write("\n")
