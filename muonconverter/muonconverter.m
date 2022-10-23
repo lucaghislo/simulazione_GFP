62379b44eebf0f55286fcd2076a440b0c3307a86
Luca Ghislotti <ghislottiluca@gmail.com>
2022-10-23 15:38:30 +0200

    Added muonconverter function code

--- muonconverter/muonconverter.m
+++ muonconverter/muonconverter.m
@@ -11,11 +11,12 @@
 %    landau_flag: (true/false) plot with or without landau fit
 %         fig_on: (true/false) show or hide plot figure
 %      bin_width: histogram bin width
+%        max_kev: max value in keV for landau and/or plot  on x
 
 % OUTPUT
 % muon_allch_out: muon data output after conversion (in keV)
 
-function [muon_allch_out] = muonconverter(data_in_path, pt, ch_start, ch_finish, plot_out_path, landau_flag, fig_on, bin_width)
+function [muon_allch_out] = muonconverter(data_in_path, pt, ch_start, ch_finish, plot_out_path, landau_flag, fig_on, bin_width, max_kev)
 
     if ch_start>ch_finish
         error("Channel range error! Starting channel must be greater than finishin channel.")
@@ -49,7 +50,7 @@ function [muon_allch_out] = muonconverter(data_in_path, pt, ch_start, ch_finish,
     f = figure("Visible", fig_on_flag);
     
     if landau_flag
-        histfitlandau(muon_allch(muon_allch>100), bin_width, 0, 6000, 1); % bin_width = 15
+        histfitlandau(muon_allch(muon_allch>100), bin_width, 0, max_kev, 1); % bin_width = 15
     else
         histogram(muon_allch, "DisplayStyle", "stairs", 'BinWidth', bin_width, 'LineWidth', 1); % bin_width = 20
         set(gca, 'YScale', 'log')
@@ -59,7 +60,7 @@ function [muon_allch_out] = muonconverter(data_in_path, pt, ch_start, ch_finish,
     
     box on
     grid on
-    xlim([0, 6000])
+    xlim([0, max_kev])
     ylabel("\textbf{Counts}")
     xlabel("\textbf{Incoming energy [keV]}")
     set(gca,'FontSize', 12)
