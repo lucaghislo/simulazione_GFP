0b23cd7e9a7ddd95d5876e4f6425760e5f212bd8
Luca Ghislotti <l.ghislotti@studenti.unibg.it>
2022-10-27 11:25:51 +0200

    Updated GFO data plot

--- acquire_event_data.py
+++ acquire_event_data.py
@@ -8,11 +8,11 @@ edep_values = event_data["edep_values"]
 for row in range(0, 6):
     for mod in range(0, 6):
         file = open(
-            "GFP_Data/events/EDEP/row"
+            "GFP_Data/events/ADU/row"
             + str(row)
             + "_mod"
             + str(mod)
-            + "_allch_EDEP.dat",
+            + "_allch_ADU.dat",
             "w+",
         )
 
@@ -28,5 +28,7 @@ for row in range(0, 6):
             if len(list) > 0:
                 for item in list:
                     file.write(str(item) + ", ")
-
-                file.write("\n")
+            else:
+                file.write("")
+            
+            file.write("\n")
