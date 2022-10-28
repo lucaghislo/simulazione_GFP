5dd76a88ac53117f639320636254693fd3f2a90e
Luca Ghislotti <ghislottiluca@gmail.com>
2022-10-28 11:53:58 +0200

    Added single channel files for ADU energy deposition

--- acquire_event_data.py
+++ acquire_event_data.py
@@ -7,16 +7,19 @@ edep_values = event_data["edep_values"]
 
 for row in range(0, 6):
     for mod in range(0, 6):
-        file = open(
-            "GFP_Data/events/ADU/row"
-            + str(row)
-            + "_mod"
-            + str(mod)
-            + "_allch_ADU.dat",
-            "w+",
-        )
-
         for ch in range(0, 32):
+
+            file = open(
+                "GFP_Data/events/ADU/single_channels/row"
+                + str(row)
+                + "_mod"
+                + str(mod)
+                + "_ch"
+                + str(ch)
+                + "_ADU.dat",
+                "w+",
+            )
+
             array = np.array(edep_values[:, row, mod, ch])
             count = 0
             list = []
@@ -27,8 +30,8 @@ for row in range(0, 6):
 
             if len(list) > 0:
                 for item in list:
-                    file.write(str(item) + ", ")
+                    file.write(str(item) + "\n")
             else:
                 file.write("")
-            
-            file.write("\n")
+
+            # file.write("\n")
