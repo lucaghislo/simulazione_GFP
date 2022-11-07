95b63aaa548004f6e908b9084a5f918fe8f7fa15
Luca Ghislotti <ghislottiluca@gmail.com>
2022-11-07 15:26:12 +0100

    Uploaded FDT analysis for modules on row

--- GFP_analisi_FDT.m
+++ GFP_analisi_FDT.m
@@ -56,12 +56,14 @@ end
 clearvars -except fdt_allmodules dac_values ch_values;
 clc;
 
-legend_text = nan(5, 1);
+colors = distinguishable_colors(6, 'w');
+module_count = 0;
 for row = [0:5]
     f = figure("Visible", "off");
     hold on
     for module = [0:5]
-        plot(dac_values.*0.841, fdt_allmodules(:, ch_count + 1).*0.841);
+        plot(dac_values.*0.841, fdt_allmodules(:, module_count + 1).*0.841, "LineWidth", 1, 'Color', [colors(module+1, 1), colors(module+1, 2), colors(module+1, 3)]);
+        module_count = module_count + 1;
     end
     hold off
     
@@ -69,56 +71,25 @@ for row = [0:5]
     grid on
     xlabel('\textbf{Incoming energy [MeV]}');
     ylabel('\textbf{Channel Output [ADU]}');
-    ylim([0 2000])
+    ylim([0 1600])
     xlim([0, 53824]);
     xticks([0:10000:50000])
     xticklabels([0:10:50])
-    yticks([0:200:2000])
+    yticks([0:200:1600])
     set(gcf, 'Color', 'w');
-    title("Mean transfer function for modules on row " + string(row));
+    title("\textbf{Mean transfer function for modules on row " + string(row) + "}");
+    hleg = legend("0", "1", "2", "3", "4", "5", "Location", "southeast");
+    htitle = get(hleg,'Title');
+    set(htitle,'String','\textbf{Module on row}')
 
     ax = gca; 
     fontsize = 12;
     ax.XAxis.FontSize = fontsize; 
     ax.YAxis.FontSize = fontsize;
+    ax.Legend.FontSize = fontsize;
     ax.Title.FontSize = fontsize + 4;
     f.Position = [0 0 1200 800];
     
-    exportgraphics(gcf, "");
-    disp("SAVED: ")
-
-
+    exportgraphics(gcf, "C:\Users\ghisl\Documents\GitHub\simulazione_GFP\output\GFP_row_analysis\GFP_FDT_row" + string(row) + ".pdf");
+    disp("SAVED: GFP_FDT_row" + string(row) + ".pdf")
 end
-
-%%
-% Plot funzione di trasferimento media per tutti i moduli divisi per row
-f = figure("Visible", "off");
-hold on
-for ch = ch_values
-    plot(dac_values.*0.841, fdt_allmodules(:, ch_count + 1).*0.841);
-    ch_count = ch_count + 1;
-end
-hold off
-
-box on
-grid on
-xlabel('\textbf{Incoming energy [MeV]}');
-ylabel('\textbf{Channel Output [ADU]}');
-ylim([0 2000])
-xlim([0, 53824]);
-xticks([0:10000:50000])
-xticklabels([0:10:50])
-yticks([0:200:2000])
-set(gcf, 'Color', 'w');
-title("")
-
-ax = gca; 
-fontsize = 12;
-ax.XAxis.FontSize = fontsize; 
-ax.YAxis.FontSize = fontsize;
-ax.Title.FontSize = fontsize + 4;
-f.Position = [0 0 1200 800];
-
-exportgraphics(gcf, "");
-disp("SAVED: ")
-
